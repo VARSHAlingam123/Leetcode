66b6282f92d2a3f7c1f12bd50c1b3772948b7aaf
VARSHAlingam123 <144180769+VARSHAlingam123@users.noreply.github.com>
2024-05-25 14:09:15 -0400

    Add files via upload

---
+++ d1.java
@@ -0,0 +1,10 @@
+package Ds;
+import java.util.*;
+public class d1{
+	public static void main(String[] Args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int k=sc.nextInt();
+		System.out.println(k);
+	}
+}
